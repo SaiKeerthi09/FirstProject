1a64f8f0c79e6a3592a2c9e2da184aec3b063239
SaiKeerthi09 <65567599+SaiKeerthi09@users.noreply.github.com>
2020-05-18 17:53:16 -0400

    Update HelloProgram.java

--- HelloProgram.java
+++ HelloProgram.java
@@ -4,5 +4,7 @@ class HelloProgram {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
         System.out.println("Hello, Sai!");
+        System.out.println("Hello, K!");
+        System.out.println("Hello, P!");
     }
-}
+} 
